5b4b5849ac39e40ddfa1af5120f52f5bae7e199a
dilshantmohan <126072409+dilshantmohan@users.noreply.github.com>
2023-02-22 17:32:55 +0530

    Update streamlit_app.py
    Diner Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('Healthy Diner :)')
+streamlit.title('Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
